327d76571be6fc87b56d7293056faffe2905785e
Kareem Belgharbi <kareembelgharbi@gmail.com>
2018-07-11 17:13:25 +0200

    attempt #55

--- scripts/game.js
+++ scripts/game.js
@@ -403,7 +403,6 @@ var lastPlayerId = cookieArray[1].substring(14);
 // Check.
 if (player_id != lastPlayerId) {
 	alert('map bug');
-	location.reload();
 }
 
 // Update the lastPlayerId as a cookie for next time.
